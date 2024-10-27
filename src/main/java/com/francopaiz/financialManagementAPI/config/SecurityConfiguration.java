145eeb9de04ec85a6186f8a55bf5898345cd853f
fpaiz-2022134 <fpaiz-2022134@kinal.edu.gt>
2024-10-27 10:39:00 -0600

    feat: Setting the routes for auth

--- src/main/java/com/francopaiz/financialManagementAPI/config/SecurityConfiguration.java
+++ src/main/java/com/francopaiz/financialManagementAPI/config/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration {
                         .requestMatchers("/api/v1/users/**").authenticated()
                         .requestMatchers("/api/v1/categories/**").authenticated()
                         .requestMatchers("/api/v1/incomes/**").authenticated()
-                        .requestMatchers("/api/v1/expenses/**").authenticated()
+                        .requestMatchers("/api/v1t/expenses/**").authenticated()
                         .requestMatchers("/api/v1/summaries/**").authenticated()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session
